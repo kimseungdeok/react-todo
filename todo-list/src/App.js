34a3909eb7fd333feba8a8f554e2738e58c08c41
kimseungdeok <tmdejr1117@gmail.com>
2023-05-12 01:04:55 +0900

    feat : form 컴포넌트 생성 및 적용

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React, { Component } from "react";
 import TodoListTemplate from "./components/TodoListTemplate";
+import Form from "./components/Form";
 
 function App() {
     return (
         <div className="App">
-            <TodoListTemplate form={<div>폼</div>}>
+            <TodoListTemplate form={<Form />}>
                 TodoListTemplate
             </TodoListTemplate>
         </div>
